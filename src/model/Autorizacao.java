e861dc6ecfdcd979dc86c76f8a97c867397d752e
jhon donavan <jhondonavan@gmail.com>
2017-10-15 09:53:19 -0200

    class Usuario and Autorizacao encoded

---
+++ src/model/Autorizacao.java
@@ -0,0 +1,58 @@
+package model;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="autorizacoes")
+public class Autorizacao implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(generator = "codigo_papel", strategy = GenerationType.SEQUENCE)
+	@SequenceGenerator(name = "codigo_papel", sequenceName = "codigo_papel", allocationSize = 1)	
+	private Integer id;
+   	
+	private String nome;
+	
+	private String descricao;
+	
+ 
+    public Autorizacao() {
+    }
+	
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+	
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+	
+
+
+}
---
+++ src/model/Usuario.java
@@ -0,0 +1,175 @@
+package model;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotEmpty;
+
+@Entity
+@Table(name = "usuarios")
+public class Usuario implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(generator = "codigo_usuario", strategy = GenerationType.SEQUENCE)
+	@SequenceGenerator(name = "codigo_usuario", sequenceName = "codigo_usuario", allocationSize = 1)
+	private Integer id;
+
+	@Column(name = "userName", nullable = false, unique = true)
+	private String nomeUsuario;
+
+	@Column(name = "email", nullable = false, unique = true)
+	private String email;
+
+	@Column(name = "password", nullable = false, unique = false)
+	private String senha;
+
+	@Column(name = "enable", columnDefinition = "BOOLEAN")
+	private boolean enable;
+
+
+
+	@ManyToMany
+	@NotEmpty(message = "Favor selecionar os papeis do usuário.")
+	@JoinTable(name = "autorizacao_usuario")
+	private List<Autorizacao> autorizacoes;
+
+	@Column(name = "lastAccess", unique = true)
+	@Temporal(TemporalType.DATE)
+	private Date ultimoAcesso;
+
+	public Usuario() {
+
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNomeUsuario() {
+		return nomeUsuario;
+	}
+
+	public void setNomeUsuario(String nomeUsuario) {
+		this.nomeUsuario = nomeUsuario;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public boolean isEnable() {
+		return enable;
+	}
+
+	public void setEnable(boolean enable) {
+		this.enable = enable;
+	}
+
+	public List<Autorizacao> getAutorizacoes() {
+		return autorizacoes;
+	}
+
+	public void setAutorizacoes(List<Autorizacao> autorizacoes) {
+		this.autorizacoes = autorizacoes;
+	}
+
+	public Date getUltimoAcesso() {
+		return ultimoAcesso;
+	}
+
+	public void setUltimoAcesso(Date ultimoAcesso) {
+		this.ultimoAcesso = ultimoAcesso;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((autorizacoes == null) ? 0 : autorizacoes.hashCode());
+		result = prime * result + ((email == null) ? 0 : email.hashCode());
+		result = prime * result + (enable ? 1231 : 1237);
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((nomeUsuario == null) ? 0 : nomeUsuario.hashCode());
+		result = prime * result + ((senha == null) ? 0 : senha.hashCode());
+		result = prime * result + ((ultimoAcesso == null) ? 0 : ultimoAcesso.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Usuario other = (Usuario) obj;
+		if (autorizacoes == null) {
+			if (other.autorizacoes != null)
+				return false;
+		} else if (!autorizacoes.equals(other.autorizacoes))
+			return false;
+		if (email == null) {
+			if (other.email != null)
+				return false;
+		} else if (!email.equals(other.email))
+			return false;
+		if (enable != other.enable)
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (nomeUsuario == null) {
+			if (other.nomeUsuario != null)
+				return false;
+		} else if (!nomeUsuario.equals(other.nomeUsuario))
+			return false;
+		if (senha == null) {
+			if (other.senha != null)
+				return false;
+		} else if (!senha.equals(other.senha))
+			return false;
+		if (ultimoAcesso == null) {
+			if (other.ultimoAcesso != null)
+				return false;
+		} else if (!ultimoAcesso.equals(other.ultimoAcesso))
+			return false;
+		return true;
+	}
+
+
+
+
+}
